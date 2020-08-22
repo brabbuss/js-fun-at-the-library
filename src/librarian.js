967d7fd2f33e66276b21da70e35fe83089d8e780
Scott Brabson <scottbrabson@gmail.com>
2020-08-22 16:30:11 -0600

    Complete librarian.js test

--- src/librarian.js
+++ src/librarian.js
@@ -2,10 +2,42 @@ var checkoutBook = require('../src/library.js').checkoutBook;
 var searchShelf = require('../src/shelf.js').searchShelf;
 
 class Librarian {
-  constructor() {
-
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
   }
-  
+  greetPatron(patronName, morning) {
+    if (morning == true) {
+      return 'Good morning, ' + patronName + '!'
+    } else {
+    return 'Hello, ' + patronName + '!'
+    }
+  }
+  findBook(bookTitle) {
+    for (var i = 0; i < this.library.shelves.fantasy.length; i++) {
+      if (bookTitle === this.library.shelves.fantasy[i].title) {
+        this.library.shelves.fantasy.splice([i]);
+        return 'Yes, we have ' + bookTitle
+      }
+    }
+    for (var i = 0; i < this.library.shelves.fiction.length; i++) {
+      if (bookTitle === this.library.shelves.fiction[i].title) {
+        this.library.shelves.fiction.splice([i]);
+        return 'Yes, we have ' + bookTitle
+      }
+    }
+    for (var i = 0; i < this.library.shelves.nonFiction.length; i++) {
+      if (bookTitle === this.library.shelves.nonFIction[i].title) {
+        this.library.shelves.nonFiction.splice([i]);
+        return 'Yes, we have ' + bookTitle
+      }
+    }
+    return 'Sorry, we do not have ' + bookTitle
+  }
+  calculateLateFee(days) {
+    return Math.ceil(.25 * days)
+  }
 }
+
 
 module.exports = Librarian;
